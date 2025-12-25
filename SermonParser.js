4ef1643bec7494fde66be67506c6a8953ef19088
Connor Lee <62191475+ConnorLprogram@users.noreply.github.com>
2025-12-25 14:53:04 -0800

    Add files via upload

--- SermonParser.js
+++ SermonParser.js
@@ -6,24 +6,34 @@ window.onload = function (){
     submitButton.addEventListener("click", function() {
         var inputText = textArea.value;
         var result = inputText.replace(/ +/g, " ");
-        console.log(result);
         result = result.replace(/\n+/g, "\n");
         result = result.replace(/[^\x00-\x7F]/g, "");
-        let newSlides = result.match(/\n\w{0,4}\.|\n.*\:/g);
+        let newSlides = result.match(/\n\s*\w{0,4}\.|\n.*\:/g);
 
         newSlides.forEach(newSlide => {
             let delimitedString = newSlide.replace(/\n/g, "\\\\");
             result = result.replace(newSlide, delimitedString);
         });
-      outputArea.textContent = result;
+
+        splitResult = result.split("\\\\");
+        console.log(result);
+        for (var i = 0; i < splitResult.length; i++){
+            var endString = splitResult[i].trim();
+            endString = endString.replace(/\n/, "\n\n");
+            splitResult[i] = endString;
+        }
+        result = splitResult.join("\\\\");
+        let curDate = new Date();
+        var dateText = (curDate.getMonth() + 1) + "." + curDate.getDate() + "." + curDate.getFullYear();
+        outputArea.textContent = dateText + "\n" + result;
     });
 }
 
-//Example input
+// Example input
 // Title
-// iiii.    text
+//  iiii.    text
 // verse: verse.
-// 2.    point
+//  2.    point
 // verse: verse
 
 // verse
